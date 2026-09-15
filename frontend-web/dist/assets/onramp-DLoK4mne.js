4bd0f6a13e4592bda7b72f6fc858dadab23476c7
thaiquo <ngocquocthai.004@gmail.com>
2026-09-15 22:54:42 +0700

    fix giao diện ví và khiếu nại

--- frontend-web/dist/assets/onramp-DLoK4mne.js
+++ frontend-web/dist/assets/onramp-DLoK4mne.js
@@ -1,4 +1,4 @@
-import{B as e,D as t,E as n,M as r,V as i,b as a,g as o,j as s,l as c,r as l,t as u,w as d,y as f}from"./ModalController-DLyGHbfk.js";import{_ as p,c as m,l as h,o as g,s as _,x as v,y}from"./wui-text-BTwUa-zK.js";import{E as b,v as x}from"./index-DZhFxKBH.js";import"./wui-image-1zMk5OEE.js";var S=p`
+import{B as e,D as t,E as n,M as r,V as i,b as a,g as o,j as s,l as c,r as l,t as u,w as d,y as f}from"./ModalController-DLyGHbfk.js";import{_ as p,c as m,l as h,o as g,s as _,x as v,y}from"./wui-text-BTwUa-zK.js";import{E as b,v as x}from"./index-C9kmFmDt.js";import"./wui-image-C7UamdtH.js";var S=p`
   :host > wui-grid {
     max-height: 360px;
     overflow: auto;
